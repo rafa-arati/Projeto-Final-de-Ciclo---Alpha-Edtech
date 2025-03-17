083c2c2bded19c9dbe1c8ad73ee119b95fc76936
alielsonfp <alielson_pequeno@hotmail.com>
2025-03-17 19:52:32 -0300

    Adicionado logs para testar erro na VM

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,13 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+// Verifique se as variáveis de ambiente estão sendo carregadas corretamente
+console.log("Host:", process.env.DB_HOST);
+console.log("Port:", process.env.DB_PORT);
+console.log("User:", process.env.DB_USER);
+console.log("Password:", process.env.DB_PASSWORD); // Verifique a senha aqui
+console.log("Database:", process.env.DB_NAME);
+
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
