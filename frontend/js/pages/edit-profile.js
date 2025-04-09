887a238cd8673c9041fdbcb1b9438ce31cf309cd
Luis Felipe Rodrigues Bruno <lf210400@gmail.com>
2025-04-08 22:52:11 -0300

    bugfix: Corrigido bug do botao voltar na pagina de detalhes que sempre is pra home

--- frontend/js/pages/edit-profile.js
+++ frontend/js/pages/edit-profile.js
@@ -49,14 +49,10 @@ export default async function renderEditProfile(queryParams) {
         appContainer.innerHTML = `
           <div class="app-wrapper account-hub-page">
             <div class="app-container">
-
-               <header class="page-header">
-                  <button class="back-button" id="profile-back-button-back-button" title="Voltar">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 12H5M12 19l-7-7 7-7"/></svg>
-                  </button>
-                  <h1 class="page-title">Minha Conta</h1>
-                  <div class="header-placeholder"></div>
-               </header>
+              <header class="page-header">
+                <a class="back-button" id="profile-back-button" title="Voltar">${backIcon}</a>
+                <h1 id="account-hub-title">Minha Conta</h1>
+              </header>
 
               <div class="profile-content">
 
